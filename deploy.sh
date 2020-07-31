09ca23ea54024d63c94aa2382022fa725aff1fd0
Punxious <hot1989hot@hotmail.com>
2020-07-31 16:36:34 -0400

    add image Dockfile

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#TODO: check gcloud Login
+check=$(gcloud auth list)
+echo OUT:$check
+#TODO: select project
+#TODO: build docker image
+#TODO: upload image to cloud registry
+#TODO: build API with cloud run and ESP
+#TODO: build backend with cloud functions and cloud run
+#TODO: build ui with angular
