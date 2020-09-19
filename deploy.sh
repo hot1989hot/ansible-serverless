1ecf91fe6f9e4870dbb0e1f7fe187833cb2c0ab9
Punxious <hot1989hot@hotmail.com>
2020-09-19 14:05:12 -0400

    testing code

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
-#TODO: check gcloud Login
-check=$(gcloud auth list)
-echo OUT:$check
+#!/bin/bash
+#check gcloud Login
+gcloud auth list
+
 #TODO: select project
+projects=($(gcloud projects list --format="value(projectId)"))
+
+#Check APIs:
+# containerregistry.googleapis.com
+
 #TODO: build docker image
 #TODO: upload image to cloud registry
 #TODO: build API with cloud run and ESP
